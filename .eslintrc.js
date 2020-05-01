fcf9d9e20cc2aa1a11822cc564d84474a2cde611
kaaboaye <kaaboaye@gmail.com>
2020-05-01 14:25:20 +0200

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "react/jsx-one-expression-per-line": "off",
+
+    "@typescript-eslint/quotes": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+
+    "import/prefer-default-export": "off",
+
+    "no-console": "error",
+
+    "prettier/prettier": "error",
+  },
+};
